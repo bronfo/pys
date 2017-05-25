e230eadea02648ef2371ca1c09c2a35ea215dd99
bronfo <bronfo@bronfogithub.com>
2017-05-25 17:24:59 +0800

    reload3

--- apps.py
+++ apps.py
@@ -11,20 +11,21 @@ def shutdown():
     httpd.server_close()
     app.restart()
 
-'''
 class WebRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
+        '''
         if self.path == '/reset':
             self.wfile.write("reset ok\n")
             threading.Timer(0.1, shutdown).start()
         else:
             self.wfile.write("hi4\n")
-'''
+        '''
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 def start():
     global httpd
     SocketServer.TCPServer.allow_reuse_address = True
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    Handler = WebRequestHandler
     httpd = SocketServer.TCPServer(("", PORT), Handler)
     print "serving at port", PORT
     httpd.serve_forever()
