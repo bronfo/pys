b8a3ef857f4ec6dd7726c342223a44f0e0676003
bronfo <bronfo@bronfogithub.com>
2017-05-25 17:13:59 +0800

    reload

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import apps
 
+def restart():
+    reload(apps)
+    apps.start()
+
 if __name__ == "__main__":
     apps.start()
--- apps.py
+++ apps.py
@@ -1,12 +1,28 @@
 import SimpleHTTPServer
 import SocketServer
+import threading
+import app
 
 PORT = 8080
 httpd = None
 
+def shutdown():
+    httpd.shutdown()
+    httpd.server_close()
+    app.restart()
+
+class WebRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/reset':
+            self.wfile.write("reset ok\n")
+            threading.Timer(0.1, shutdown).start()
+        else:
+            self.wfile.write("hi4\n")
+
 def start():
     global httpd
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    SocketServer.TCPServer.allow_reuse_address = True
+    Handler = WebRequestHandler
     httpd = SocketServer.TCPServer(("", PORT), Handler)
     print "serving at port", PORT
     httpd.serve_forever()
